45e8a5fe2950cebe2fe0bdc70c5a02c4b9f55566
Elizaveta Prokhorova <lisa-bella97@mail.ru>
2019-12-09 19:31:31 +0300

    Fix bug

--- source/main.cpp
+++ source/main.cpp
@@ -9,18 +9,25 @@ int main(int argc, char** argv) {
     int channels = 1;
     int bits = 16;
     int sampleRate = 44100;
-    int time = 4;
+    int time = 5;
     std::string fileName = "../saved/default.wav";
 
     const std::string helpText = "Usage:\n" + std::string(argv[0]) + "\n"
-                                 "[--device {device name}]\n"
-                                 "[--channels {1 or 2}]\n"
-                                 "[--bits {8, 16 or 32 bits}]\n"
-                                 "[--rate {record rate in Hz}]\n"
-                                 "[--time {record time in seconds}]\n"
-                                 "[--file {file name (*.wav)}]\n";
+                                 "[--device <device name>]\n"
+                                 "[--channels <1|2>]\n"
+                                 "[--bits <8|16|32>]\n"
+                                 "[--rate <record rate in Hz>]\n"
+                                 "[--time <record time in seconds>]\n"
+                                 "[--file <file name (*.wav)>]\n\n"
+                                 "Default options:\n"
+                                 "--device default_device\n"
+                                 "--channels 1\n"
+                                 "--bits 16\n"
+                                 "--rate 44100\n"
+                                 "--time 5\n"
+                                 "--file \"../saved/default.wav\"\n";
 
-    for (auto i = 1; i < argc; i++) {
+    for (auto i = 1; i < argc; i += 2) {
         if (std::strcmp(argv[i], "--device") == 0)
             deviceName = argv[i + 1];
         else if (std::strcmp(argv[i], "--channels") == 0)
@@ -32,7 +39,7 @@ int main(int argc, char** argv) {
         else if (std::strcmp(argv[i], "--time") == 0)
             time = std::atoi(argv[i + 1]);
         else if (std::strcmp(argv[i], "--file") == 0)
-            fileName = argv[i + 1];
+            fileName = "../saved/" + std::string(argv[i + 1]);
         else if (std::strcmp(argv[i], "--help") == 0) {
             std::cout << helpText;
             return 0;
